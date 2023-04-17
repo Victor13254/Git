c2861a03669a58716502da623f8cdfc1480bf4f6
Victor13254 <victor.cespedes0710@gmail.com>
2023-04-17 04:18:46 -0500

    Swagger 0.1v

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,34 @@
+package org.example.controller;
+
+import org.example.model.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user/{id}")
+    public UserDto byId(@PathVariable("id") int id) {
+        return new UserDto(id, "Name " + id, "Surname " + id);
+    }
+
+    @PostMapping("/api/user/")
+    public UserDto create(@RequestBody UserDto user) {
+        return user;
+    }
+
+    @PutMapping("/api/user/")
+    public UserDto update(@RequestBody UserDto user) {
+        return user;
+    }
+
+    @PatchMapping("/api/user/")
+    public UserDto change(@RequestBody UserDto user) {
+        return user;
+    }
+
+    @DeleteMapping("/api/user/{id}")
+    public boolean delete(@PathVariable("id") int id) {
+        return true;
+    }
+
+}
